45eaefd69138c280f07adfa9f6b8f54c97fdc1ca
Ixac <ixacarrrows@gmail.com>
2021-12-07 16:15:07 +0700

    lab9 2 333

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -36,7 +36,7 @@ int main(){
 	string name[N];
 	float score[N];	
 	
-	//Store names and scores of students into an array eiei
+	//Store names and scores of students into an array 
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		
